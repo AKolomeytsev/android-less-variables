62ae6e667d64a1eb46c776d16b7939646ca5e85e
AKolomeytsev <a.kolomeytsev@list.ru>
2022-11-14 19:35:56 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte varByte = 127;
+        short varShot = 32000;
+        int varInt = 2000000000;
+        long varLong = 1000000000000L;
+
+        float varFloat = 12.45f;
+        double varDouble = 123.567;
+
+        char varChar = 'S';
+
+        boolean varBool = true;
+    }
+}
